5d28fdf0c7c1b305724e9e465234670581665651
Gurdev S. Panue <gspanue@gmail.com>
2019-11-26 15:48:37 +0000

    Created scale object with device id

--- pi/main.py
+++ pi/main.py
@@ -2,8 +2,8 @@
 
 from scale import Scale
 
-# Set DT, SCK, and calibration factor respectively
-scale = Scale(5, 6, 412)
+# Set Device ID, DT, SCK, and calibration factor respectively
+scale = Scale("d2axeD4FLhGnqvw8UPwwdV", 5, 6, 358)
 
 # Start the scale
 scale.start()
