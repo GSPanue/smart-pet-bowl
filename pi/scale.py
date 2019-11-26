eb008d1f6bd0beb9d2b3409a90a7df6d723d49a4
Gurdev S. Panue <gspanue@gmail.com>
2019-11-26 15:46:19 +0000

    Check device has been bound to an account

--- pi/scale.py
+++ pi/scale.py
@@ -4,20 +4,35 @@ import time
 import sys
 import RPi.GPIO as GPIO
 from hx711 import HX711
+from database import Database
 
 class Scale:
-  def __init__(self, dt, sck, calibration_factor = None):
+  def __init__(self, device_id, dt, sck, calibration_factor = None):
+    self.db = Database()
     self.hx = HX711(dt, sck)
+    self.device_id = device_id
     self.calibration_factor = 1
 
     if calibration_factor is not None:
       self.calibration_factor = calibration_factor
+
+  def set_device_id(self, device_id):
+    self.device_id = device_id
+
+  def get_device_id(self):
+    return self.device_id
 
   def set_calibration_factor(self, calibration_factor):
     self.calibration_factor = calibration_factor
 
   def get_calibration_factor(self):
     return self.calibration_factor
+
+  # Check if device has been bound to an account
+  def has_bound_device(self):
+    return self.db.get("Devices", {
+      "id": self.get_device_id()
+    })["account_id"] != "null"
 
   # Get current weight in the form of an integer
   def get_weight(self):
@@ -38,6 +53,18 @@ class Scale:
   # Start the scale
   def start(self):
     try:
+      # Check if device has been bound to an account
+      print("Checking if device has been bound to an account...")
+      has_bound_device = self.has_bound_device()
+
+      while not has_bound_device:
+        print("Device has not been bound to an account.")
+        print("Checking again in 60 seconds...")
+
+        time.sleep(60)
+
+        has_bound_device = self.has_bound_device()
+
       print("Preparing scales. Do not add weight to the scale...")
       self.configure_hx()
 
@@ -65,5 +92,5 @@ class Scale:
   def stop(self):
     self.clean_up()
 
-    print("Stopping program...")
+    print("Stopping the program...")
     sys.exit()
