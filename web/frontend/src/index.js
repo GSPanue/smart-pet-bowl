90e567379059499a1f09333501d99f9f972df9c0
Gurdev S. Panue <gpanue@hotmail.com>
2020-03-01 02:05:05 +0000

    Configured frontend

---
+++ web/frontend/src/index.js
@@ -0,0 +1,15 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import App from '@/App';
+import store from '@/store';
+import routes from '@/router';
+
+Vue.use(VueRouter);
+
+export default new Vue({
+  store,
+  el: '#app',
+  components: { App },
+  router: new VueRouter({ mode: 'history', routes }),
+  template: '<App/>'
+});
