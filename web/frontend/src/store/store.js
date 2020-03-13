a8e9ede205c78bfe1b6ab7062012fcd00f910d03
gspanue <gpanue@hotmail.com>
2020-03-13 23:17:31 +0000

    Added device to vuex store

--- web/frontend/src/store/store.js
+++ web/frontend/src/store/store.js
@@ -5,14 +5,21 @@ Vue.use(Vuex);
 
 const store = new Vuex.Store({
   state: {
+    device: null,
     pet: null
   },
   getters: {
+    getDevice: ({ device }) => (
+      device
+    ),
     getPet: ({ pet }) => (
       pet
     )
   },
   mutations: {
+    setDevice: (store, newDevice) => {
+      store.device = newDevice
+    },
     setPet: (store, newPet) => {
       store.pet = newPet;
     }
