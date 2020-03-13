5caef3b849214d32c8a134bd126101704c2fc595
gspanue <gpanue@hotmail.com>
2020-03-13 19:44:22 +0000

    Configured vue-axios

--- web/frontend/src/index.js
+++ web/frontend/src/index.js
@@ -1,11 +1,14 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
+import VueAxios from 'vue-axios';
+import Axios from 'axios';
 import App from '@/App';
 import store from '@/store';
 import routes from '@/router';
 import { Row, Col, Card, Form, FormItem, Input, Button, Dialog } from 'element-ui';
 
 Vue.use(VueRouter);
+Vue.use(VueAxios, Axios);
 Vue.use(Row);
 Vue.use(Col);
 Vue.use(Card);
