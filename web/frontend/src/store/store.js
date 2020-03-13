422ab44707afc77a2f9fa9a5e4c28a648d51709e
gspanue <gpanue@hotmail.com>
2020-03-13 22:54:19 +0000

    Added pet to Vuex state

--- web/frontend/src/store/store.js
+++ web/frontend/src/store/store.js
@@ -4,9 +4,19 @@ import Vuex from 'vuex';
 Vue.use(Vuex);
 
 const store = new Vuex.Store({
-  state: {},
-  getters: {},
-  mutations: {}
+  state: {
+    pet: null
+  },
+  getters: {
+    getPet: ({ pet }) => (
+      pet
+    )
+  },
+  mutations: {
+    setPet: (store, newPet) => {
+      store.pet = newPet;
+    }
+  }
 });
 
 export default store;
