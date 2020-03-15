d23c2c8554fe8031ed649f966070ba8cb2f0d218
gspanue <gpanue@hotmail.com>
2020-03-15 01:52:43 +0000

    Removed store commits

--- web/frontend/src/helpers/vuex/vuex.js
+++ web/frontend/src/helpers/vuex/vuex.js
@@ -1,13 +1,11 @@
 const createWebSocketPlugin = (socket) => (
-  (store) => {
+  () => {
     socket.onopen = () => {
       console.log('[WS]: Connected to WebSocket');
-      store.commit('setConnected', true);
     }
 
     socket.onclose = () => {
       console.log('[WS]: Disconnected from WebSocket');
-      store.commit('setConnected', false);
     }
   }
 );
