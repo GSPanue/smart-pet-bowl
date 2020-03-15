52c0faa91c4a8ab64dbb204ecd1b6c340c4f606e
gspanue <gpanue@hotmail.com>
2020-03-15 22:35:55 +0000

    Implemented handler for new readings

--- web/frontend/src/helpers/websocket/websocket.js
+++ web/frontend/src/helpers/websocket/websocket.js
@@ -1,3 +1,5 @@
+import { sortByDate } from '@/helpers';
+
 const createWebSocket = () => (
   new WebSocket(process.env.api)
 );
@@ -6,12 +8,22 @@ const handleSetConnection = (store, message) => {
   store.commit('setConnected', message);
 };
 
+const handleSendReading = (store, message) => {
+  const currentReadings = store.state.readings;
+
+  store.commit('setReadings', sortByDate([
+    ...currentReadings,
+    ...message
+  ]));
+};
+
 const createWebSocketHandlers = (store, socket) => {
   socket.onopen = () => {
     console.log('[WS]: Connected to WebSocket');
 
     const deviceId = store.state.device.id;
 
+    console.log('[WS]: Sent message with action `setConnection`');
     socket.send(JSON.stringify({
       action: 'setConnection',
       message: deviceId
@@ -26,11 +38,14 @@ const createWebSocketHandlers = (store, socket) => {
   socket.onmessage = ({ data }) => {
     const { action, message } = JSON.parse(data);
 
-    console.log(`[WS]: Received message for action \`${action}\``);
+    console.log(`[WS]: Received message with action \`${action}\``);
 
     switch (action) {
       case 'setConnection':
         handleSetConnection(store, message);
+        break;
+      case 'sendReading':
+        handleSendReading(store, message);
         break;
     }
   }
