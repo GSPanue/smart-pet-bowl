5f7bfbd2e9e6e153d0b37f2e61a2e36da66265c3
gspanue <gpanue@hotmail.com>
2020-03-15 14:07:33 +0000

    Created createWebSocketHandler helper

--- web/frontend/src/helpers/websocket/websocket.js
+++ web/frontend/src/helpers/websocket/websocket.js
@@ -2,6 +2,19 @@ const createWebSocket = () => (
   new WebSocket(process.env.api)
 );
 
+const createWebSocketHandlers = (store, socket) => {
+  socket.onopen = () => {
+    console.log('[WS]: Connected to WebSocket');
+    store.commit('setConnected', true);
+  }
+
+  socket.onclose = () => {
+    console.log('[WS]: Disconnected from WebSocket');
+    store.commit('setConnected', false);
+  }
+}
+
 export {
-  createWebSocket
+  createWebSocket,
+  createWebSocketHandlers
 };
